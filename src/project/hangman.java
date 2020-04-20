55f2c3a3c3e1c9c506454385243d9702d4e4b81f
jagrith <jh1285@wildcats.unh.edu>
2020-04-20 16:39:56 -0400

    commiting code with sprint1 user stories

---
+++ src/project/hangman.java
@@ -0,0 +1,43 @@
+package project;
+
+import java.util.Scanner;
+
+public class hangman {
+
+	private static String[] words = {"java", "program", "computer"};
+	private static String word = words[(int) (Math.random() * words.length)];
+	private static String dash = new String(new char[word.length()]).replace("\0", ".");
+	private static int count = 0;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		while (count < 4 && dash.contains(".")) {
+			System.out.println("Guess any letter in the word");
+			System.out.println(dash);
+			String guess = sc.next();
+			hang(guess);
+		}
+		sc.close();
+	}
+
+	public static void hang(String guess) {
+		String newdash = "";
+		for (int i = 0; i < word.length(); i++) {
+			if (word.charAt(i) == guess.charAt(0)) {
+				newdash += guess.charAt(0);
+			} else if (dash.charAt(i) != '.') {
+				newdash += word.charAt(i);
+			} else {
+				newdash += ".";
+			}
+		}
+
+		if (dash.equals(newdash)) {
+			count++;
+		} else {
+			dash = newdash;
+		}
+		
+	}
+}
